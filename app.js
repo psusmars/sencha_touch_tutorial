b2c34893f0c78ab81d71bae7f62454b2cb73101c
Paul Susmarski <Paul.susmarski@gmail.com>
2017-08-29 14:24:44 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+/*
+ * This file is generated and updated by Sencha Cmd. You can edit this file as
+ * needed for your application.
+ */
+Ext.application({
+    name: 'MyApp',
+    extend: "MyApp.Application",
+
+    requires: [
+        'MyApp.*'
+    ]
+
+    launch: function () {
+        Ext.Msg.alert('Hello Ext JS', 'Hello! Welcome to Ext JS.');
+    }
+});
---
+++ app/Application.js
@@ -0,0 +1,5 @@
+Ext.define('MyApp.Application', {
+    extend: 'Ext.app.application',
+
+    mainView: 'MyApp.view.main.Main'
+});
