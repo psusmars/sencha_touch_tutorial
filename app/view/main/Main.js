6395e6f691436da6c30520258951e7345a74fcb0
Paul Susmarski <Paul.susmarski@gmail.com>
2017-08-29 14:57:53 -0600

    Add a viewmodel for a text field that takes the user's username

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -2,15 +2,32 @@ Ext.define('MyApp.view.main.Main',{
     extend: 'Ext.tab.Panel',
     controller: 'main',
     requires: [
-        'Ext.Button'
+        'Ext.Button',
+        'Ext.field.Text'
     ],
+
+    viewModel: {
+        data: {
+            userName: ''
+        }
+    },
+
     items: [{
         title: 'Home',
         html: '<h1 class="main-banner">Hello World!</h1>',
         items: [{
+            xtype: 'textfield',
+            label: 'User name',
+            bind: '{userName}'
+        },
+        {
             xtype: 'button',
             text: 'Go',
-            handler: 'onGo'
+            handler: 'onGo',
+            bind: {
+                disabled: '{!userName}',
+                text: '{userName ? "Save: " + userName : "Save"}'
+            }
         }]
     }, {
         title: 'Notifications'
