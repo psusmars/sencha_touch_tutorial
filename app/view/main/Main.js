a4afc2fc136c5562f8d64b3bd2d3e0471cbd4ee0
Paul Susmarski <Paul.susmarski@gmail.com>
2017-08-29 14:49:44 -0600

    Create the main view controller that shows a message box

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -1,5 +1,6 @@
 Ext.define('MyApp.view.main.Main',{
     extend: 'Ext.tab.Panel',
+    controller: 'main',
     requires: [
         'Ext.Button'
     ],
@@ -8,7 +9,8 @@ Ext.define('MyApp.view.main.Main',{
         html: '<h1 class="main-banner">Hello World!</h1>',
         items: [{
             xtype: 'button',
-            text: 'Go'
+            text: 'Go',
+            handler: 'onGo'
         }]
     }, {
         title: 'Notifications'
